6744e32a0e2e545d6618c7b1b0ee5e5a7a050f5d
Iury Gama <Iuryempregos@gmail.com>
2024-03-23 16:31:53 -0300

    feat: Start_Project

---
+++ src/app/page.tsx
@@ -0,0 +1,74 @@
+
+import FeaturedSession from "@/shared/components/FeaturedSession";
+import { BoxArrows, BoxCarroselItems, BoxContent, ContainerGrid } from "@/shared/components/FeaturedSession/styled";
+import Slider from "@/shared/components/Slider";
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
+import { SessionCategories } from "@/shared/components/DepartamentBox/styled";
+import DepartamentBox from "@/shared/components/DepartamentBox";
+import getAllProducts from "@/shared/actions/getAllProducts";
+import ProductCard from "@/shared/components/ProductCard";
+
+export default async function Home() {
+  const products = await getAllProducts();
+
+  const categories = [
+    {
+      title: "Hardware",
+      image_url: "https://s2-ge.glbimg.com/UmcC4TyoRoi_fHaXUuU7PEEv3Ng=/0x0:1200x675/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2021/k/f/4gDXVBR8iLNA2GTd8acA/foto-nvidia-rtx-3090.png"
+    },
+    {
+      title: "Periféricos",
+      image_url: "https://assets.corsair.com/image/upload/f_auto,q_auto/v1/akamai/pdp/hs70-bluetooth/images/HS70_Bluetooth_02%202.png"
+    },
+    {
+      title: "Jogos / Games",
+      image_url: "https://images6.alphacoders.com/926/926723.jpg"
+    },
+    {
+      title: "Outros",
+      image_url: "https://cdn.shoppub.io/cdn-cgi/image/w=1000,h=1000,q=80,f=auto/oficinadosbits/media/uploads/produtos/foto/mrapkoeh/file.png"
+    },
+  ]
+
+  return (
+    <main>
+      <Slider autoSlide autoSlideInterval={5000} />
+
+      <FeaturedSession title="Promoção Semana do consumidor">
+        <BoxContent>
+          <BoxArrows>
+            <FaAngleLeft size={50} />
+          </BoxArrows>
+          
+          <BoxCarroselItems>
+            {products?.map(product => <ProductCard key={product.id} path={product.id} product={product} /> )}
+          </BoxCarroselItems>
+          
+          <BoxArrows>
+            <FaAngleRight size={50} />
+          </BoxArrows>
+        </BoxContent>
+      </FeaturedSession>
+
+
+      <FeaturedSession title="Navegar por Categoria">
+        <SessionCategories>
+            {categories.map((category, index) => {
+              const { title, image_url } = category;
+              return(
+                <DepartamentBox key={index} image_description={title} title={title} image_url={image_url} path="" />
+              )
+            })}
+        </SessionCategories>
+      </FeaturedSession>
+
+
+      <FeaturedSession title="Mais Procurados" >
+        <ContainerGrid>
+        {products?.map(product => <ProductCard key={product.id} path={product.id} product={product} /> )}
+        </ContainerGrid>
+      </FeaturedSession>
+
+    </main>
+  );
+}
---
+++ src/styles/main.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { createGlobalStyle } from 'styled-components';
+
+ 
+const GlobalStyle = createGlobalStyle`
+  :root{
+    --primary-color: #D10024;
+    --secondary-color: #E4E7ED;
+    --button-color: #ef233c;
+    --background-color: #15161D;
+    --text-primary-color: #2B2D42;
+    --subtext-color: #8D99AE;
+  }
+
+
+  *{
+    padding: 0;
+    margin: 0;
+    text-decoration: none;
+    list-style: none;
+    box-sizing: border-box;
+  }
+  body {
+    background: aliceblue;
+    min-height: 100dvh;
+  }
+`;
+ 
+export default GlobalStyle;
