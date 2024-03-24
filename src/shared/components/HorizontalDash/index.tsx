b125f4082277c11b0ebecf8c33e99d31273e1942
Iury Gama <Iuryempregos@gmail.com>
2024-03-24 15:00:32 -0300

    feat: create Dash Component

---
+++ src/shared/components/HorizontalDash/index.tsx
@@ -0,0 +1,15 @@
+
+import styled from "styled-components";
+
+const StyledDash = styled.div`
+    width: 15%;
+    height: 2px;
+    background-color: var(--secondary-color);
+    border-radius: 8px;
+`
+
+export default function HorizontalDash() {
+    return <StyledDash />
+}
+
+ HorizontalDash;
